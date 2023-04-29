825d48d5f15cb724c06bb63b32f96750c311363e
therealjarbean <jarbean.ipad@gmail.com>
2023-04-29 11:40:04 -0400

    Define Node and TreeNode classes

--- nodes.py
+++ nodes.py
@@ -1,2 +1,10 @@
-class TreeNode:
-    def __init__:
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+class TreeNode(Node):
+    def __init__(self, data, leftChild = None, rightChild = None):
+        self.data = data
+        self.leftChild = leftChild
+        self.rightChild = rightChild
