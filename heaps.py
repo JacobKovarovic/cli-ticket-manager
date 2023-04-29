ff4b0b53a14970f0e8d0a7b2819f27829fa2c259
therealjarbean <jarbean.ipad@gmail.com>
2023-04-29 11:51:29 -0400

    Modify AbstractHeap __init__

--- heaps.py
+++ heaps.py
@@ -1,14 +1,18 @@
+from nodes import TreeNode
+
 class AbstractHeap():
     def __init__(self, sourceCollection = None):
         self.size = 0
+        self.head = None
         if sourceCollection != None:
             if not iter(sourceCollection):
                 raise TypeError("Source collection must be iterable")
             for item in sourceCollection:
-                self.add()
+                self.add(item)
 
     def __str__(self):
         return str(map(str, self))
 
-    def __add__(self, item):
-        self.push(item)
+class MinHeap(AbstractHeap):
+    def __init__(self, sourceCollection = None):
+        super(sourceCollection)
