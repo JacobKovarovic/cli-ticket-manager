a969e7e60d8b4f6ce631422b33917b39ce04d11d
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 16:30:33 -0400

    checkLogin -> check_login

--- src/check_login.py
+++ src/check_login.py
@@ -1,6 +1,6 @@
 from sys import exit
 
-def checkLogin():
+def check_login():
     session = open("session.json", 'r')
     if session.readline() != '':
         exit("User is already signed in. Please sign out to sign in as other user.")
