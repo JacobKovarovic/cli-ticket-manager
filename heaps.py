68dcbc1e526a9a9add5da0a3d2e0003ca8464741
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-29 20:41:36 -0400

    Finish implementing push, pop, and peek for MinHeap
    Also added a few miscellaneous helper methods to both MinHeap and AbstractHeap associated with this functionality

--- heaps.py
+++ heaps.py
@@ -11,30 +11,130 @@ class _AbstractHeap():
                 self.push(item)
 
     def __str__(self):
-        return str(map(str, self))
+        if self.isEmpty():
+            raise KeyError("Heap is empty")
+        return self._toStringPreorder(self.head)
+
+    def _toStringPreorder(self, currNode):
+        if currNode == None:
+            return ""
+        if currNode.leftChild == None and currNode.rightChild == None:
+            return str(currNode.data)
+        return str(currNode.data) + "(" + self._toStringPreorder(currNode.leftChild) + ", " + self._toStringPreorder(currNode.rightChild) + ")"
+
+    def isEmpty(self):
+        return self.head == None
+
+    def getDeepestParent(self):
+        if self.isEmpty():
+            raise KeyError("Heap is empty")
+        if self.head.getHeight() == 1:
+            return self.head
+        return self._getDeepestParent(self.head)
+    
+    def _getDeepestParent(self, currNode):
+        if currNode.leftChild == None and currNode.rightChild == None:
+            return None
+        if currNode.rightChild == None:
+            return currNode
+        if currNode.isComplete():
+            getRightmost = lambda node: node if node.rightChild.rightChild == None else getRightmost(node.rightChild)
+            deepestParent = getRightmost(currNode)
+            return deepestParent
+        deepestParent = self._getDeepestParent(currNode.rightChild)
+        if deepestParent == None:
+            deepestParent = self._getDeepestParent(currNode.leftChild)
+        return deepestParent
+
+    def _removeDeepest(self):
+        if self.getDeepestParent().rightChild == None:
+            self.getDeepestParent().leftChild = None
+            return
+        self.getDeepestParent().rightChild = None
+
+    def getDeepestNode(self):
+        deepestParent = self.getDeepestParent()
+        if deepestParent.rightChild == None:
+            return deepestParent.leftChild
+        return deepestParent.rightChild
 
 class MinHeap(_AbstractHeap):
     def __init__(self, sourceCollection = None):
-        super(sourceCollection)
+        super().__init__(sourceCollection)
 
     def push(self, item):
-        if self.head == None:
+        self.size += 1
+        if self.isEmpty():
             self.head = TreeNode(item)
             return
-        _pushNode(self.head, TreeNode(item))
+        self._push(self.head, TreeNode(item))
 
-    def _pushNode(self, currNode, newNode):
-        if type(newNode) != type(TreeNode()):
-            raise TypeError("Cannot push non-TreeNode object")
+    def _push(self, currNode, newNode):
         if currNode.leftChild == None:
+            if newNode.data < currNode.data:
+                tempData = currNode.data
+                currNode.data = newNode.data
+                self._push(self.head, TreeNode(tempData))
+                return
             currNode.leftChild = newNode
             return
         if currNode.rightChild == None:
+            if newNode.data < currNode.data:
+                tempData = currNode.data
+                currNode.data = newNode.data
+                self._push(self.head, TreeNode(tempData))
+                return
             currNode.rightChild = newNode
+            return
+        if newNode.data < currNode.data:
+            tempData = currNode.data
+            currNode.data = newNode.data
+            self._push(self.head, TreeNode(tempData))
             return
         leftHeight = currNode.leftChild.getHeight()
         rightHeight = currNode.rightChild.getHeight()
         if leftHeight == rightHeight:
-            self._pushNode(currNode.leftChild, newNode)
+            self._push(currNode.leftChild, newNode)
+            return
         if leftHeight > rightHeight:
-            self._pushNode(currNode.rightChild, newNode)
+            self._push(currNode.rightChild, newNode)
+
+    def pop(self):
+        if self.isEmpty():
+            return KeyError("Heap is empty")
+        returnItem = self.head.data
+        if self.head.getHeight() == 0:
+            self.head = None
+            return returnItem
+        deepestNode = self.getDeepestNode()
+        self.head.data = deepestNode.data
+        self._removeDeepest()
+        self._reheapify(self.head)
+        self.size -= 1
+        return returnItem
+
+    def _reheapify(self, currNode):
+        if currNode.leftChild == None and currNode.rightChild == None:
+            return
+        if currNode.rightChild == None:
+            if currNode.leftChild.data < currNode.data:
+                temp = currNode.data
+                currNode.data = currNode.leftChild.data
+                currNode.leftChild.data = temp
+            return
+        if currNode.leftChild.data < currNode.rightChild.data:
+            if currNode.leftChild.data < currNode.data:
+                temp = currNode.data
+                currNode.data = currNode.leftChild.data
+                currNode.leftChild.data = temp
+                return self._reheapify(currNode.leftChild)
+            return
+        if currNode.rightChild.data < currNode.data:
+            temp = currNode.data
+            currNode.data = currNode.rightChild.data
+            currNode.rightChild.data = temp
+            return self._reheapify(currNode.rightChild)
+        return
+
+    def peek(self):
+        return self.head.data
--- nodes.py
+++ nodes.py
@@ -1,17 +1,33 @@
-class Node():
-    def __init__(self, data, next = None):
-        self.data = data
-        self.next = next
-
-class TreeNode(Node):
+class TreeNode():
     def __init__(self, data, leftChild = None, rightChild = None):
         self.data = data
         self.leftChild = leftChild
         self.rightChild = rightChild
 
     def getHeight(self):
-        if self.leftChild = None and self.rightChild = None:
+        if self.leftChild == None and self.rightChild == None:
             return 0
-        if self.leftChild.getHeight() > self.rightChild.getHeight()
+        if self.leftChild == None:
+            return 1 + self.rightChild.getHeight()
+        if self.rightChild == None:
+            return 1 + self.leftChild.getHeight()
+        if self.leftChild.getHeight() > self.rightChild.getHeight():
             return 1 + self.leftChild.getHeight()
         return 1 + self.rightChild.getHeight()
+
+    def clone(self):
+        return TreeNode(self.data)
+    
+    def __str__(self):
+        return str(self.data) + "(" + str(self.leftChild) + ", " + str(self.rightChild) + ")"
+    
+    def isComplete(self):
+        if self.getHeight() == 0:
+            return True
+        if self.leftChild == None:
+            return False
+        if self.rightChild == None:
+            return False
+        if self.leftChild.getHeight() == self.rightChild.getHeight():
+            return True and self.leftChild.isComplete() and self.rightChild.isComplete()
+        return False
