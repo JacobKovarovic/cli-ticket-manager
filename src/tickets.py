e575e750839c52d78bb8ca758bf600c00b220a76
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 14:47:15 -0400

    Update __eq__ to test for equality of instance variables

--- src/tickets.py
+++ src/tickets.py
@@ -11,7 +11,24 @@ class Ticket:
         self.closed = False
 
     def __lt__(self, other):
-        return 
-
+        if type(other) != type(self):
+            raise TypeError("Only Tickets can be compared to Tickets")
+        return self.priority < other.priority
+    
+    def __gt__(self, other):
+        if type(other) != type(self):
+            raise TypeError("Only Tickets can be compared to Tickets")
+        return self.priority > other.priority
+    
+    def __eq__(self, other):
+        if type(other) != type(self):
+            raise TypeError("Only Tickets can be compared to Tickets")
+        return self.title == other.title and \
+               self.descriptio == other.description and \
+               self.tasks == other.tasks and \
+               self.priority == other.priority and \
+               self.creationDate == other.creationDate and \
+               self.closed == other.closed
+    
     def isClosed(self):
         return not self.closed
