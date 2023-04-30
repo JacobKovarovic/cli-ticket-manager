843fb360ea55c7a4441a535307f91e59eb47d112
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 16:31:12 -0400

    Close file after reading

--- src/check_login.py
+++ src/check_login.py
@@ -3,4 +3,6 @@ from sys import exit
 def check_login():
     session = open("session.json", 'r')
     if session.readline() != '':
+        session.close()
         exit("User is already signed in. Please sign out to sign in as other user.")
+    session.close()
