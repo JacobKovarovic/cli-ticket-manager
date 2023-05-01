cc60ae7beb7cd04d90811a56eed945ab1307bde3
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 23:37:51 -0400

    Don't print closed tickets unless arg is passed

--- list_tickets.py
+++ list_tickets.py
@@ -3,16 +3,20 @@ from src.queues import PriorityQueue
 import sys
 
 check_logged_out()
+valid_arguments = ['-t', '-tasks', '-c', '-closed']
 usage = "Usage:\nlist_tickets.py\n\t-t -tasks (Print all tasks in all tickets)"
 printTasks = False
 
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     raise KeyError("Too many arguments.\n" + usage)
 if len(sys.argv) > 1:
-    if sys.argv[1].rstrip() not in ['-t', '-tasks']:
-        raise ValueError("Invalid argument.\n" + usage)
-    else:
-        printTasks = True
+    for arg in sys.argv:
+        if arg not in valid_arguments:
+            raise ValueError("Invalid argument.\n" + usage)
+        if arg in ['-t', '-tasks']:
+            printTasks = True
+        if arg in ['-c', '-closed']:
+            printClosed = True
 
 allTickets = load_tickets()
 pq = PriorityQueue()
@@ -21,6 +25,9 @@ if allTickets == []:
     print("No tickets to list.")
 else:
     for ticket in allTickets:
+        if not printClosed():
+            if ticket.isClosed():
+                continue
         pq.enqueue(ticket)
     while not pq.isEmpty():
         if printTasks:
