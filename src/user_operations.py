b0d03e0690f809b837b84cea932ed09840a78a73
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 20:27:11 -0400

    Add check for if no user is logged in

--- src/user_operations.py
+++ src/user_operations.py
@@ -1,15 +1,22 @@
 from sys import exit
 import json
 
-def check_login():
+def check_logged_in():
     session = open("session.json", 'r')
     if session.readline() != '':
         session.close()
         exit("User is already signed in. Please sign out to sign in as other user.")
     session.close()
 
+def check_logged_out():
+    session = open("session.json", 'r')
+    if session.readline() == '':
+        session.close()
+        exit("You are not signed in. Please sign in to perform this operation.")
+    session.close()
+
 def get_loggedin_user():
-    check_login()
+    check_logged_out()
     session = open("session.json", 'r')
     user = json.loads(session.readline())
     session.close()
