65a0058c21ff80583918be8fd52051a86c099d05
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 23:09:15 -0400

    Add list_claimed_tasks functionality

---
+++ list_claimed_tasks.py
@@ -0,0 +1,14 @@
+from textwrap import indent
+from src.user_operations import check_logged_out, load_tickets, get_loggedin_user
+
+check_logged_out()
+user = get_loggedin_user()
+allTickets = load_tickets()
+if allTickets == []:
+    raise ValueError("No tickets.")
+
+for ticket in allTickets:
+    if not ticket.isClosed() and ticket.getNextTask().getOwner() == user[0]:
+        task = ticket.getNextTask()
+        print(task.getParentTicket())
+        print(indent(str(task), "\t"))
