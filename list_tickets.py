c5161410808a610917c2749d5138d7225d2cdf58
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 23:15:09 -0400

    List tickets by priority instead of in order

--- list_tickets.py
+++ list_tickets.py
@@ -1,9 +1,14 @@
 from src.user_operations import check_logged_out, load_tickets
+from src.queues import PriorityQueue
 
 check_logged_out()
 allTickets = load_tickets()
+pq = PriorityQueue()
+print()
 if allTickets == []:
     print("No tickets to list.")
 else:
     for ticket in allTickets:
-        print(ticket)
+        pq.enqueue(ticket)
+    while not pq.isEmpty():
+        print(pq.dequeue())
