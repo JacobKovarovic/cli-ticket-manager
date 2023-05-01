a56e5e8a625c9d68e0df9a39924a435673485b55
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 23:32:01 -0400

    List only ticket names unless arg is set
    Printing > 1 ticket on the command line with all tasks is far too verbose to be useful in most cases. Add argument "-t" or "-tasks" to run with original behaviour

--- list_tickets.py
+++ list_tickets.py
@@ -1,7 +1,19 @@
 from src.user_operations import check_logged_out, load_tickets
 from src.queues import PriorityQueue
+import sys
 
 check_logged_out()
+usage = "Usage:\nlist_tickets.py\n\t-t -tasks (Print all tasks in all tickets)"
+printTasks = False
+
+if len(sys.argv) > 2:
+    raise KeyError("Too many arguments.\n" + usage)
+if len(sys.argv) > 1:
+    if sys.argv[1].rstrip() not in ['-t', '-tasks']:
+        raise ValueError("Invalid argument.\n" + usage)
+    else:
+        printTasks = True
+
 allTickets = load_tickets()
 pq = PriorityQueue()
 print()
@@ -11,4 +23,7 @@ else:
     for ticket in allTickets:
         pq.enqueue(ticket)
     while not pq.isEmpty():
-        print(pq.dequeue())
+        if printTasks:
+            print(pq.dequeue())
+        else:
+            print(pq.dequeue().getTitle())
