1320059a57bc6c8ce0d67708943d64af1b802403
TheRealJarbean <jaron.e.anderson@gmail.com>
2023-04-30 23:04:49 -0400

    Store ticket after being claimed

--- claim_task.py
+++ claim_task.py
@@ -4,7 +4,7 @@ Postcondition: Set task to claimed by user if task is not claimed
 """
 
 import sys
-from src.user_operations import check_logged_out, load_tickets, get_loggedin_user
+from src.user_operations import check_logged_out, load_tickets, store_tickets, get_loggedin_user
 
 check_logged_out()
 user = get_loggedin_user()
@@ -12,6 +12,8 @@ if (len(sys.argv) != 2):
     raise ValueError('Incorrect number of arguments: Usage "check_active_tasks.py "<ticketName>"')
 
 allTickets = load_tickets()
+if allTickets == []:
+    raise ValueError("There are no tickets.")
 key = sys.argv[1]
 for ticket in allTickets:
     if not ticket.isClosed() and ticket.getTitle() == key:
@@ -29,6 +31,7 @@ for ticket in allTickets:
         else:
             ticket.getNextTask().setOwner(user[0])
             print(ticket.getNextTask())
+            store_tickets(allTickets)
             sys.exit("Task claimed.")
 
 raise KeyError("No open ticket with that name found.")
