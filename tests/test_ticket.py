271e2136d4e9184ab0a2a6e76812fc795467bd36
Jacob K <jakekovarovic@gmail.com>
2024-03-16 21:29:54 -0500

    refactor: implemented pytest for full Ticket class

--- tests/test_ticket.py
+++ tests/test_ticket.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pytest
 sys.path.insert(1, os.path.join(sys.path[0], '..')) 
+from datetime import date, timedelta, datetime
 from src.utils.task import Task
 from src.utils.ticket import Ticket
 
@@ -20,6 +21,18 @@ def ticket2():
     tasks = [task1, task2]
     ticket = Ticket("Ticket02","Sam's Ticket",tasks,2,7)
     return ticket
+
+@pytest.fixture
+def ticket3():
+    task1 = Task("Task1", "programming", 2, 1)
+    task2 = Task("Task2", "management", 3, 2)
+    tasks = [task1, task2]
+    ticket = Ticket("Ticket03","Tina's Ticket",tasks,2,leadDays=5)
+    return ticket
+
+@pytest.fixture
+def due_date():
+    return date.today() + timedelta(days=5)
 
 @pytest.fixture
 def expected_list(ticket1):
@@ -93,3 +106,37 @@ def test_getNextTask(ticket1):
     task1 = Task("Task1", "art", 3, 1)
     # Assert that the output matches the expected title
     assert task == task1, f"Expected task to be equal to task1, but got {task}"
+
+#Test the finished task methods
+def test_finishTasks(ticket2):
+    #test unfinishedTasks method
+    unfinished_tasks = ticket2.getUnfinishedTasks()
+    task1 = Task("Task1", "sound", 4, 1)
+    task2 = Task("Task2", "management", 3, 2)
+    tasks = [task1, task2]
+    assert unfinished_tasks == tasks
+
+    #Finish task1
+    ticket2.finishNextTask()
+    task1.finishTask()
+    finish_t = [task1]
+
+    #Test getFinishedTasks method
+    finished_tasks = ticket2.getFinishedTasks()
+    assert finished_tasks == finish_t,f"Expected {finish_t}, but got {finished_tasks}"
+
+def test_daysTilDue(ticket3):
+    #For ticket 3 the leadDays = 5
+    days_til_due = ticket3.daysTilDue()
+    
+    # Assert that the returned value matches the expected number of days until the due date
+    assert days_til_due == 5, f"Expected 5 days until due, but got {days_til_due}" 
+
+def test_Closed(ticket2):
+    #Checking ticket status
+    status = ticket2.isClosed()
+    assert status == False, f"Expected ticket to be open, but getting {status}"
+    #Closing ticket and checking status
+    ticket2.close()
+    status = ticket2.isClosed()
+    assert status, f"Expected ticket to be closed, but getting {status}"
